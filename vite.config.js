83b176fd2f1fbdc98125ab1175275066ed55fc61
Rajesh SIngh <38010721+rajcse1721@users.noreply.github.com>
2026-03-29 22:00:18 +0530

    Change base path in Vite config to lowercase

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   // This matches your repository name exactly
-  base: "/HR_OUTREACH/",
+  base: "/hroutreach/",
   plugins: [react()],
   server: {
     port: 4000,
