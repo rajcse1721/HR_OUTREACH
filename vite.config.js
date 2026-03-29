d693745e60caff3503f8fad029211923908f4430
Rajesh SIngh <38010721+rajcse1721@users.noreply.github.com>
2026-03-29 22:24:42 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   // This matches your repository name exactly
-  base: "/hroutreach/",
+  base: "/HR_OUTREACH/",
   plugins: [react()],
   server: {
     port: 4000,
